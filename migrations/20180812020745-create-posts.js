be5dfc242e94039d1db71a7f08916c829327aa10
fufu976 <fufu976@gmail.com>
2018-08-12 23:07:17 +0800

    Add migration files to fix model schema

---
+++ migrations/20180812020745-create-posts.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        type: Sequelize.STRING
+      },
+      article: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      }
+    }, {charset: 'utf8',collate: 'utf8_unicode_ci'});
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Posts');
+  }
+};
